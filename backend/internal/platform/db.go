cba672c860cb510790f4743e5fdfb4833f1f606d
Javier Villanueva <oscar.villanueva.prieto@gmail.com>
2025-11-25 17:18:26 -0600

    refactor(db.go): convert the connection to a singleton

--- backend/internal/platform/db.go
+++ backend/internal/platform/db.go
@@ -2,26 +2,71 @@ package platform
 
 import (
 	"fmt"
-	"log"
+	"sync"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 )
 
-type DBConnection struct {
+var lock = &sync.Mutex{}
+var SharedInstance *gorm.DB
+
+type dbConnection struct {
 	Host string
 	Database string
 	User string
 }
 
-func (db DBConnection) Connect() {
+func (db dbConnection) Connect() (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", db.User, db.Host, db.Database)
 
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	if err != nil {
-		log.Panicf("Failed to connect database: %v", err)
+	return connection, err
+}
+
+func GetInstance() {
+	if SharedInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+
+		if SharedInstance == nil {
+			log.Info("Creating a db instance")
+
+			err := godotenv.Load()
+
+			dbHost := os.Getenv("DB_HOST")
+			dbName := os.Getenv("DB_DATABASE")
+			dbUser := fmt.Sprintf("%s:%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
+
+			if err != nil {
+				log.Warning("Could't load env: ", err)
+				return
+			}
+
+			dbConn := dbConnection {
+				Host: dbHost,
+				Database: dbName,
+				User: dbUser,
+			}
+
+			db, err := dbConn.Connect()
+
+			if err != nil {
+				log.Warning("Couldn't connect to the database: ", err)
+				return
+			}
+
+			SharedInstance = db
+
+			log.Info("Instance created")
+		} else {
+			log.Info("Instance already created")
+		}
+	} else {
+		log.Info("Instance already created")
 	}
-
-	log.Println("Connected to the database successfully!")
 }
