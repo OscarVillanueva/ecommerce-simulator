c2981ce94c3f7a73517c221b830d381cee4d0476
Javier Villanueva <oscar.villanueva.prieto@gmail.com>
2025-12-31 17:16:28 -0600

    feat(validatejwt.go): prepare the function of validate a token

---
+++ backend/internal/app/tools/validateToken.go
@@ -0,0 +1,37 @@
+package tools
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func IsValidToken(tokenString string) (map[string]any, error)  {
+	manager := getKeyManager()
+
+	if manager.PublicKey == nil {
+		return nil, errors.New("Public key not found")
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Check if the token has a method pointer of type ECDSA
+		// variable.(Type) is called Type assertion like a typeof of JS
+		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return manager.PublicKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Verify that the claims is a map
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("Invalid token")
+}
