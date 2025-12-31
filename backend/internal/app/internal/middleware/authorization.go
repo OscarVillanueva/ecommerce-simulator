882dc87e4dde6d53d02c456a15b99d6e48dab96f
Javier Villanueva <oscar.villanueva.prieto@gmail.com>
2025-12-31 17:48:27 -0600

    feat(authorization.go): prepare the middleware to check if the request has a token

---
+++ backend/internal/app/internal/middleware/authorization.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/OscarVillanueva/goapi/internal/app/tools"
+)
+
+// Define a complex string key to avoid collitions in the context
+type contextKey string
+const UserUUIDKey contextKey = "user_uuid"
+
+func Authorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
+		authHeader := r.Header.Get("Authorization")
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			tools.UnauthorizedErrorHandler(w)
+			return
+		}
+
+		claims, err := tools.IsValidToken(parts[1])
+		if err != nil {
+			tools.UnauthorizedErrorHandler(w)
+			return
+		}
+
+		uuid, ok := claims["uuid"].(string)
+		if !ok {
+			tools.UnauthorizedErrorHandler(w)
+			return
+		}
+
+		// Create a context based in the request context
+		ctx := context.WithValue(r.Context(), UserUUIDKey, uuid)
+		modified := r.WithContext(ctx)
+
+		next.ServeHTTP(w, modified)
+	})
+}
