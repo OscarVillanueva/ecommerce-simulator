2221ec81598149c1c2403e0f9d6d67b5c786e58c
Javier Villanueva <oscar.villanueva.prieto@gmail.com>
2026-01-08 17:16:00 -0600

    fix(productrepository.go): remove the logs

--- backend/internal/app/internal/db/productRepository.go
+++ backend/internal/app/internal/db/productRepository.go
@@ -11,14 +11,12 @@ import (
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
-	log "github.com/sirupsen/logrus"
 )
 
 func InsertProduct(product *requests.CreateProduct, belongTo string, ctx context.Context) (*dao.Product, error) {
 	db := platform.GetInstance()
 
 	if db == nil {
-		log.Error("We couldn't connect to the database")
 		return nil, errors.New("We couldn't connect to the database")
 	}
 
@@ -34,7 +32,6 @@ func InsertProduct(product *requests.CreateProduct, belongTo string, ctx context
 	}
 
 	if err := gorm.G[dao.Product](db).Create(ctx, &p); err != nil {
-		log.Error(err)
 		return nil, err
 	}
 
