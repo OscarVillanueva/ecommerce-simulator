ed39eddee87088b00d386db71b9cce7b63d80c62
Javier Villanueva <oscar.villanueva.prieto@gmail.com>
2026-01-08 17:19:05 -0600

    fix(createproduct.go): add a validation method to check each prop

--- backend/internal/app/models/requests/CreateProduct.go
+++ backend/internal/app/models/requests/CreateProduct.go
@@ -1,7 +1,24 @@
 package requests
 
+import "errors"
+
 type CreateProduct struct {
 	Name string `json:"name"`
 	Price float32 `json:"price"`
 	Quantity int32 `json:"quantity`
 }
+
+func (c *CreateProduct) Validate() error {
+	if c.Name == "" {
+		return errors.New("Product name is required")
+	}
+	if c.Price < 0 {
+		return errors.New("Price cannot be negative")
+	}
+	if c.Quantity < 0 {
+		return errors.New("Quantity cannot be negative")
+	}
+
+	return nil
+}
+
