8d9c540b0471af8c65701872584624d276b8f633
Javier Villanueva <oscar.villanueva.prieto@gmail.com>
2026-02-19 16:45:16 -0600

    build(productrepository.go): send information to alloy and grafana

--- backend/internal/app/internal/db/productRepository.go
+++ backend/internal/app/internal/db/productRepository.go
@@ -13,6 +13,7 @@ import (
 	"github.com/OscarVillanueva/goapi/internal/platform"
 
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -22,10 +23,17 @@ var ErrProductNotFound = errors.New("Product Not Found")
 var RepositoryName = "product-repository"
 
 func InsertProduct(product *requests.CreateProduct, belongTo string, ctx context.Context) (*dao.Product, error) {
+	tr := otel.Tracer(RepositoryName)
+	trContext, span := tr.Start(ctx, fmt.Sprintf("%s.InsertProduct", RepositoryName))
+	defer span.End()
+
 	db := platform.GetInstance()
 
 	if db == nil {
-		return nil, errors.New("We couldn't connect to the database")
+		dbErr := errors.New("We couldn't connect to the database")
+		span.RecordError(dbErr)
+		span.SetStatus(codes.Error, dbErr.Error())
+		return nil, dbErr
 	}
 
 	p := dao.Product{
@@ -39,37 +47,59 @@ func InsertProduct(product *requests.CreateProduct, belongTo string, ctx context
 		UpdatedAt: nil,
 	}
 
-	if err := gorm.G[dao.Product](db).Create(ctx, &p); err != nil {
+	if err := gorm.G[dao.Product](db).Create(trContext, &p); err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return nil, err
 	}
-
+	
+	span.SetStatus(codes.Ok, "InsertProduct success")
 	return &p, nil
 }
 
 func DeleteProduct(uuid string, user string, ctx context.Context) error  {
+	tr := otel.Tracer(RepositoryName)
+	trContext, span := tr.Start(ctx, fmt.Sprintf("%s.DeleteProduct", RepositoryName))
+	defer span.End()
+
 	db := platform.GetInstance()
 
 	if db == nil {
-		return errors.New("We couldn't connect to the database")
+		dbErr := errors.New("We couldn't connect to the database")
+		span.RecordError(dbErr)
+		span.SetStatus(codes.Error, dbErr.Error())
+		return dbErr
 	}
 
-	result := db.WithContext(ctx).Where("uuid = (?) AND belongs_to = (?)", uuid, user).Delete(&dao.Product{})
+	result := db.WithContext(trContext).Where("uuid = (?) AND belongs_to = (?)", uuid, user).Delete(&dao.Product{})
 	if result.Error != nil {
+		span.RecordError(result.Error)
+		span.SetStatus(codes.Error, result.Error.Error())
 		return result.Error
 	}
 
 	if result.RowsAffected == 0 {
+		span.RecordError(ErrProductNotFound)
 		return ErrProductNotFound
 	}
+
+	span.SetStatus(codes.Ok, "DeleteProduct successfully")
 
 	return nil
 }
 
 func UpdateProduct(productID string, product *requests.CreateProduct, belongTo string, ctx context.Context) error  {
+	tr := otel.Tracer(RepositoryName)
+	trContext, span := tr.Start(ctx, fmt.Sprintf("%s.DeleteProduct", RepositoryName))
+	defer span.End()
+
 	db := platform.GetInstance()
 
 	if db == nil {
-		return errors.New("We couldn't connect to the database")
+		dbErr := errors.New("We couldn't connect to the database")
+		span.RecordError(dbErr)
+		span.SetStatus(codes.Error, dbErr.Error())
+		return dbErr
 	}
 
 	updatedProduct := map[string]interface{}{
@@ -80,11 +110,13 @@ func UpdateProduct(productID string, product *requests.CreateProduct, belongTo s
 	}
 
 	result := db.Model(&dao.Product{}).
-		WithContext(ctx).
+		WithContext(trContext).
 		Where("uuid = (?) AND belongs_to = (?)", productID, belongTo).
 		Updates(updatedProduct)
 	
 	if result.Error != nil {
+		span.RecordError(result.Error)
+		span.SetStatus(codes.Error, result.Error.Error())
 		return result.Error
 	}
 
@@ -105,6 +137,7 @@ func GetProducts(params parameters.GetProductsParams) (*requests.ProductsRespons
 	if db == nil {
 		dbErr := errors.New("We couldn't connect to the database")
 		span.RecordError(dbErr)
+		span.SetStatus(codes.Error, dbErr.Error())
 		return nil, dbErr
 	}
 	
@@ -144,31 +177,49 @@ func GetProducts(params parameters.GetProductsParams) (*requests.ProductsRespons
 		Pages: int(math.Ceil(float64(count) / float64(limit))),
 	}
 
+	span.SetStatus(codes.Ok, "GetProducts successfully")
+
 	return &response, err
 }
 
 func GetProduct(user string, productId string, ctx context.Context) (*dao.Product, error) {
-	db := platform.GetInstance()
+	tr := otel.Tracer(RepositoryName)
+	trContext, span := tr.Start(ctx, fmt.Sprintf("%s.GetProducts", RepositoryName))
+	defer span.End()
 
+	db := platform.GetInstance()
 	if db == nil {
-		return nil, errors.New("We couldn't connect to the database")
+		dbErr := errors.New("We couldn't connect to the database")
+		span.RecordError(dbErr)
+		span.SetStatus(codes.Error, dbErr.Error())
+		return nil, dbErr
 	}
 	
 	var product dao.Product
-	err := db.WithContext(ctx).Where("belongs_to = (?) AND uuid = (?)", user, productId).First(&product).Error
+	err := db.WithContext(trContext).Where("belongs_to = (?) AND uuid = (?)", user, productId).First(&product).Error
 
 	if errors.Is(err, gorm.ErrRecordNotFound) {
+		span.RecordError(ErrProductNotFound)
+		span.SetStatus(codes.Error, ErrProductNotFound.Error())
 		return nil, ErrProductNotFound
 	}
+
+	span.SetStatus(codes.Ok, "GetProduct successfully")
 
 	return &product, err
 }
 
 func UpdateProductImage(productID string, path string, userID string, ctx context.Context) error {
-	db := platform.GetInstance()
+	tr := otel.Tracer(RepositoryName)
+	trContext, span := tr.Start(ctx, fmt.Sprintf("%s.GetProducts", RepositoryName))
+	defer span.End()
 
+	db := platform.GetInstance()
 	if db == nil {
-		return errors.New("We couldn't connect to the database")
+		dbErr := errors.New("We couldn't connect to the database")
+		span.RecordError(dbErr)
+		span.SetStatus(codes.Error, dbErr.Error())
+		return dbErr
 	}
 
 	updatedProduct := map[string]interface{}{
@@ -176,17 +227,22 @@ func UpdateProductImage(productID string, path string, userID string, ctx contex
 	}
 
 	result := db.Model(&dao.Product{}).
-		WithContext(ctx).
+		WithContext(trContext).
 		Where("uuid = (?) AND belongs_to = (?)", productID, userID).
 		Updates(updatedProduct)
 	
 	if result.Error != nil {
+		span.RecordError(result.Error)
+		span.SetStatus(codes.Error, result.Error.Error())
 		return result.Error
 	}
 
 	if result.RowsAffected == 0 {
+		span.RecordError(ErrProductNotFound)
 		return ErrProductNotFound
 	}
+
+	span.SetStatus(codes.Ok, "UpdateProductImage successfully")
 
 	return nil
 }
