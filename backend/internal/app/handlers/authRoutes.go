5df0b072aba07f328f43406993f8f0aee69a45ca
Javier Villanueva <oscar.villanueva.prieto@gmail.com>
2026-02-20 14:46:55 -0600

    ops: start sending logs to alloy service

--- backend/internal/app/handlers/authRoutes.go
+++ backend/internal/app/handlers/authRoutes.go
@@ -13,33 +13,47 @@ import (
 	"github.com/OscarVillanueva/goapi/internal/app/internal/db"
 	"github.com/OscarVillanueva/goapi/internal/platform"
 
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel"
 	"github.com/go-chi/chi/v5"
-	log "github.com/sirupsen/logrus"
 	mysql "github.com/go-sql-driver/mysql"
 )
+
+const AuthRouterName = "auth-router"
 
 func AuthRouter(router chi.Router) {
 	router.Post("/create-account", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "application/json")
 
+		tr := otel.Tracer(AuthRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s/create-account", AuthRouterName))
+		defer span.End()
+
 		account := requests.CreateAccount{}
 		if err := json.NewDecoder(r.Body).Decode(&account); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, errors.New("Invalid body request"))
 			return
 		}
 
-		token, err := db.CreateAccount(account, r.Context())
+		token, err := db.CreateAccount(account, ctx)
 		if err != nil  {
 			var mysqlErr *mysql.MySQLError
 
 			if errors.As(err, &mysqlErr) && mysqlErr.Number == 1062 {
-				log.Error(err)
+				span.SetAttributes(
+					attribute.String("Email", account.Email),
+					attribute.String("Name", account.Name),
+					attribute.String("Message", "The email already exists"),
+				)
 				tools.UnprocessableContent(w, "The email already exists")
 				return
 			}
 
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
@@ -47,10 +61,16 @@ func AuthRouter(router chi.Router) {
 		to := []string{account.Email}
 		msg := []byte(fmt.Sprintf("Use this token to verify your account: %s", token))
 		if err := platform.SendEmail(to, msg); err != nil {
-			log.Warning("Couldn't send the email: ", err)
+			span.SetAttributes(
+				attribute.String("Email", account.Email),
+			)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.ServiceUnavailableErrorHandler(w)
 			return
 		}
+
+		span.SetStatus(codes.Ok, "Created Account")
 
 		resp := tools.Message {
 			Message: "Token sended to the given email, please verify the account",
@@ -63,30 +83,54 @@ func AuthRouter(router chi.Router) {
 	router.Post("/verify-account", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
+		tr := otel.Tracer(AuthRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s/verify-account", AuthRouterName))
+		defer span.End()
+
 		var verify requests.VerifyAccount
 		if err := json.NewDecoder(r.Body).Decode(&verify); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, errors.New("Invalid body request"))
 			return
 		}
 
 		var magic dao.Magic
-		if err := db.FindMagicLinkForUser(verify.Token, verify.Email, &magic, r.Context()); err != nil {
+		if err := db.FindMagicLinkForUser(verify.Token, verify.Email, &magic, ctx); err != nil {
+			span.SetAttributes(
+				attribute.String("Token", verify.Token),
+				attribute.String("Email", verify.Email),
+			)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.UnprocessableContent(w, "The token or email are invalid")
 			return
 		}
 
 		if magic.ExpirationDate.Before(time.Now().UTC()) {
 			msg := "Expired Token"
+			span.SetAttributes(
+				attribute.String("Token", verify.Token),
+			)
+			span.RecordError(errors.New(msg))
+			span.SetStatus(codes.Error, msg)
 			tools.UnauthorizedErrorHandler(w, &msg)
 			return
 		}
 	
-		if err := db.VerifyUserAccount(magic, r.Context()); err != nil {
-			log.Error("We couldnt delete the token")
+		if err := db.VerifyUserAccount(magic, ctx); err != nil {
+			span.SetAttributes(
+				attribute.String("Token", magic.Token),
+				attribute.String("BelongsTo", magic.BelongsTo),
+				attribute.String("ExpirationDate", magic.ExpirationDate.String()),
+			)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
+
+		span.SetStatus(codes.Ok, "Verified Account")
 
 		resp := tools.Message {
 			Message: "Verified account",
@@ -99,15 +143,22 @@ func AuthRouter(router chi.Router) {
 	router.Post("/resend-code", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "application/json")
 
+		tr := otel.Tracer(AuthRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s/resend-code", AuthRouterName))
+		defer span.End()
+
 		var resend requests.ResendCode
 		if err := json.NewDecoder(r.Body).Decode(&resend); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, errors.New("Invalid body request"))
 			return
 		}
 
 		var user dao.User
-		if err := db.FetchUser(resend.Email, &user, r.Context()); err != nil {
+		if err := db.FetchUser(resend.Email, &user, ctx); err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			resp := tools.Message {
 				Message: "If an account exists with this email, a code has been sent",
 				Data: "success",
@@ -118,7 +169,9 @@ func AuthRouter(router chi.Router) {
 		}
 
 		var magic dao.Magic
-		if err := db.RegenerateMagicLink(r.Context(), user.Uuid, &magic); err != nil {
+		if err := db.RegenerateMagicLink(ctx, user.Uuid, &magic); err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
@@ -126,10 +179,14 @@ func AuthRouter(router chi.Router) {
 		to := []string{resend.Email}
 		msg := []byte(fmt.Sprintf("Use this token to verify your account: %s", magic.Token))
 		if err := platform.SendEmail(to, msg); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
+
+
+		span.SetStatus(codes.Ok, "ResendCode success")
 
 		resp := tools.Message {
 			Message: "If an account exists with this email, a code has been sent",
@@ -142,35 +199,62 @@ func AuthRouter(router chi.Router) {
 	router.Post("/login", func(w http.ResponseWriter, r *http.Request){		
 		w.Header().Set("Content-Type", "application/json")
 
+		tr := otel.Tracer(AuthRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s/login", AuthRouterName))
+		defer span.End()
+
 		var login requests.Login
 		if err := json.NewDecoder(r.Body).Decode(&login); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, errors.New("Invalid body request"))
 			return
 		}
 
 		var user dao.User
-		if err := db.FetchUser(login.Email, &user, r.Context()); err != nil {
+		if err := db.FetchUser(login.Email, &user, ctx); err != nil {
 			msg := "The email or token are invalid"
+			span.SetAttributes(
+				attribute.String("Email", login.Email),
+			)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.UnauthorizedErrorHandler(w, &msg)
 			return
 		}
 
 		if !user.Verified {
 			msg := "The email or token are invalid"
+			span.SetAttributes(
+				attribute.Bool("Verified", user.Verified),
+				attribute.String("UserUuid", user.Uuid),
+			)
+			span.RecordError(errors.New(msg))
+			span.SetStatus(codes.Error, msg)
 			tools.UnauthorizedErrorHandler(w, &msg)
 			return
 		}
 
 		var magic dao.Magic
-		if err := db.FetchMagicLink(user.Uuid, login.Token, &magic, r.Context()); err != nil {
+		if err := db.FetchMagicLink(user.Uuid, login.Token, &magic, ctx); err != nil {
 			msg := "The email or token are invalid"
+			span.SetAttributes(
+				attribute.String("Token", login.Token),
+				attribute.String("UserUuid", user.Uuid),
+			)
+			span.RecordError(errors.New(msg))
+			span.SetStatus(codes.Error, msg)
 			tools.UnauthorizedErrorHandler(w, &msg)
 			return
 		}
 
 		if magic.ExpirationDate.Before(time.Now().UTC()) {
 			msg := "The email or token are invalid"
+			span.SetAttributes(
+				attribute.String("ExpirationDate", magic.ExpirationDate.String()),
+			)
+			span.RecordError(errors.New(msg))
+			span.SetStatus(codes.Error, msg)
 			tools.UnauthorizedErrorHandler(w, &msg)
 			return
 		}
@@ -180,14 +264,19 @@ func AuthRouter(router chi.Router) {
 			map[string]any{ "uuid": user.Uuid, "name": user.Name })
 
 		if err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
 
-		if err := db.DeleteMagicLink(user.Uuid, r.Context()); err != nil {
-			log.Error(err)
+		if err := db.DeleteMagicLink(user.Uuid, ctx); err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			return
 		}
+
+		span.SetStatus(codes.Ok, "Login success")
 
 		resp := tools.Message {
 			Message: token,
--- backend/internal/app/handlers/productosRoutes.go
+++ backend/internal/app/handlers/productosRoutes.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"errors"
 	"strings"
 	"strconv"
@@ -15,29 +16,51 @@ import (
 	"github.com/OscarVillanueva/goapi/internal/app/tools"
 	"github.com/OscarVillanueva/goapi/internal/platform"
 	
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel"
 	"github.com/go-chi/chi/v5"
 	log "github.com/sirupsen/logrus"
 )
+
+const ProductsRouterName = "products-router"
 
 func ProductsRouter(router chi.Router)  {
 	router.Use(middleware.Authorization)
 
 	router.Get("/", func (w http.ResponseWriter, r *http.Request) {
+		tr := otel.Tracer(ProductsRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s.GET./", ProductsRouterName))
+		defer span.End()
+
 		w.Header().Set("Content-Type", "application/json")
 		page := 1
 		onlyAvailable := false
 
-		userID, ok := r.Context().Value(middleware.UserUUIDKey).(string)
+		userID, ok := ctx.Value(middleware.UserUUIDKey).(string)
 		if !ok || userID == ""{
+			err := errors.New("Missing user uuid")
+			span.SetAttributes(
+				attribute.String("uuid", userID),
+			)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.UnauthorizedErrorHandler(w, nil)
 			return
 		}
 
 		pageStr := r.URL.Query().Get("page")
+
+		span.SetAttributes(
+			attribute.String("PageStr", pageStr),
+		)
+
 		if pageStr != "" {
 			parsedPage, err := strconv.Atoi(pageStr)
 
 			if err != nil || parsedPage <= 0 {
+				span.RecordError(err)
+				span.SetStatus(codes.Error, err.Error())
 				tools.BadRequestErrorHandler(w, errors.New("Invalid Page number"))
 				return
 			}
@@ -46,10 +69,17 @@ func ProductsRouter(router chi.Router)  {
 		}
 
 		availableStr := r.URL.Query().Get("available")
+
+		span.SetAttributes(
+			attribute.String("Available", availableStr),
+		)
+
 		if availableStr != "" {
 			parsedAvailable, err := strconv.ParseBool(availableStr)
 
 			if err != nil {
+				span.RecordError(err)
+				span.SetStatus(codes.Error, err.Error())
 				tools.BadRequestErrorHandler(w, errors.New("Invalid available value"))
 				return
 			}
@@ -59,18 +89,25 @@ func ProductsRouter(router chi.Router)  {
 
 		search := r.URL.Query().Get("search")
 
+		span.SetAttributes(
+			attribute.String("Search", search),
+		)
+
 		products, err := db.GetProducts(parameters.GetProductsParams{
 			User: userID,
 			Page: page,
-			Context: r.Context(),
+			Context: ctx,
 			OnlyAvailable: onlyAvailable,
 			SearchName: search,
 		})
 		if err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
+
+		span.SetStatus(codes.Ok, "Fetch products successfully")
 
 		resp := tools.Message {
 			Message: "Products",
@@ -83,31 +120,46 @@ func ProductsRouter(router chi.Router)  {
 	router.Post("/", func (w http.ResponseWriter, r *http.Request)  {
 		w.Header().Set("Content-Type", "application/json")
 
-		uuid, ok := r.Context().Value(middleware.UserUUIDKey).(string)
+		tr := otel.Tracer(ProductsRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s.POST./", ProductsRouterName))
+		defer span.End()
+
+		uuid, ok := ctx.Value(middleware.UserUUIDKey).(string)
 		if !ok || uuid == ""{
+			err := errors.New("Missing user uuid")
+			span.SetAttributes(
+				attribute.String("uuid", uuid),
+			)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.UnauthorizedErrorHandler(w, nil)
 			return
 		}
 
 		var product requests.CreateProduct
 		if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, errors.New("Invalid body request"))
 			return
 		}
 
 		if err := product.Validate(); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, err)
 			return
 		}
 
-		result, err := db.InsertProduct(&product, uuid, r.Context())
+		result, err := db.InsertProduct(&product, uuid, ctx)
 		if err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
+
+		span.SetStatus(codes.Ok, "Create Product successfully")
 
 		resp := tools.Message {
 			Message: "The product was created successfully",
@@ -120,29 +172,54 @@ func ProductsRouter(router chi.Router)  {
 	router.Delete("/{product}", func (w http.ResponseWriter, r *http.Request)  {
 		w.Header().Set("Content-Type", "application/json")
 
-		userID, ok := r.Context().Value(middleware.UserUUIDKey).(string)
+		tr := otel.Tracer(ProductsRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s.DELETE./", AuthRouterName))
+		defer span.End()
+
+		userID, ok := ctx.Value(middleware.UserUUIDKey).(string)
+		
+		span.SetAttributes(
+			attribute.String("UserID", userID),
+		)
+
 		if !ok || userID == ""{
+			err := errors.New("Missing user uuid")
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.UnauthorizedErrorHandler(w, nil)
 			return
 		}
 
 		productID := chi.URLParam(r, "product")
+
+		span.SetAttributes(
+			attribute.String("ProductID", productID),
+		)
+
 		if strings.TrimSpace(productID) == "" {
-			tools.BadRequestErrorHandler(w, errors.New("Invalid product id"))
+			err := errors.New("Invalid product id")
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			tools.BadRequestErrorHandler(w, err)
 			return
 		}
 
-		if err := db.DeleteProduct(productID, userID, r.Context()); err != nil {
+		if err := db.DeleteProduct(productID, userID, ctx); err != nil {
 			if err.Error() == "Record Not found" {
 				tools.NotFoundErrorHandler(w, "Product not found")
 				return
 			}
+
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 
 			// Real DB error
 			log.Error("Failed to delete product: ", err)
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
+
+		span.SetStatus(codes.Ok, "Product Deleted Successfully")
 
 		resp := tools.Message {
 			Message: "The product was deleted successfully",
@@ -155,41 +232,71 @@ func ProductsRouter(router chi.Router)  {
 	router.Put("/{product}", func (w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
-		userID, ok := r.Context().Value(middleware.UserUUIDKey).(string)
+		tr := otel.Tracer(AuthRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s.PUT./", ProductsRouterName))
+		defer span.End()
+
+		userID, ok := ctx.Value(middleware.UserUUIDKey).(string)
+
+		span.SetAttributes(
+			attribute.String("UserID", userID),
+		)
+
 		if !ok || userID == ""{
+			err := errors.New("Missing user uuid")
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.UnauthorizedErrorHandler(w, nil)
 			return
 		}
 
 		productID := chi.URLParam(r, "product")
+
+		span.SetAttributes(
+			attribute.String("ProductId", productID),
+		)
+
 		if strings.TrimSpace(productID) == "" {
-			tools.BadRequestErrorHandler(w, errors.New("Invalid product id"))
+			err := errors.New("Invalid product id")
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			tools.BadRequestErrorHandler(w, err)
 			return
 		}
 
 		var product requests.CreateProduct
 		if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
-			log.Error(err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, errors.New("Invalid body request"))
 			return
 		}
 
 		if err := product.Validate(); err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, err)
 			return
 		}
 
-		if err := db.UpdateProduct(productID, &product, userID, r.Context()); err != nil {
+		if err := db.UpdateProduct(productID, &product, userID, ctx); err != nil {
 			if errors.Is(err, db.ErrProductNotFound){
+				span.RecordError(db.ErrProductNotFound)
+				span.SetStatus(codes.Error, db.ErrProductNotFound.Error())
 				tools.NotFoundErrorHandler(w, "Product not found")
 				return
 			}
+
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 
 			// Real DB error
 			log.Error("Failed to delete product: ", err)
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
+
+		span.SetStatus(codes.Ok, "Updated Successfully")
 
 		resp := tools.Message {
 			Message: "The product was updated successfully",
@@ -202,24 +309,49 @@ func ProductsRouter(router chi.Router)  {
 	router.Put("/{product}/image", func (w http.ResponseWriter, r *http.Request)  {
 		w.Header().Set("Content-Type", "application/json")
 
-		userID, ok := r.Context().Value(middleware.UserUUIDKey).(string)
+		tr := otel.Tracer(AuthRouterName)
+		ctx, span := tr.Start(r.Context(), fmt.Sprintf("%s/create-account", AuthRouterName))
+		defer span.End()
+
+		userID, ok := ctx.Value(middleware.UserUUIDKey).(string)
+
+		span.SetAttributes(
+			attribute.String("UserID", userID),
+		)
+
 		if !ok || userID == ""{
+			err := errors.New("Missing user uuid")
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.UnauthorizedErrorHandler(w, nil)
 			return
 		}
 
 		productID := chi.URLParam(r, "product")
+
+		span.SetAttributes(
+			attribute.String("ProductId", productID),
+		)
+		
 		if strings.TrimSpace(productID) == "" {
-			tools.BadRequestErrorHandler(w, errors.New("Invalid product id"))
+			err := errors.New("Invalid product id")
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			tools.BadRequestErrorHandler(w, err)
 			return
 		}
 
-		_, err := db.GetProduct(userID, productID, r.Context())
+		_, err := db.GetProduct(userID, productID, ctx)
 		if err != nil {
 			if errors.Is(err, db.ErrProductNotFound){
+				span.RecordError(db.ErrProductNotFound)
+				span.SetStatus(codes.Error, db.ErrProductNotFound.Error())
 				tools.NotFoundErrorHandler(w, "Product not found")
 				return
 			}
+
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 
 			log.Error("Failed to delete product: ", err)
 			tools.InternalServerErrorHandler(w, nil)
@@ -231,6 +363,8 @@ func ProductsRouter(router chi.Router)  {
 
 		file, handler, err := r.FormFile("image")
 		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			tools.BadRequestErrorHandler(w, errors.New("We couldn't get the image"))
 			return
 		}
@@ -239,31 +373,43 @@ func ProductsRouter(router chi.Router)  {
 		
 		contentType := handler.Header.Get("Content-Type")
 		if contentType != "image/jpeg" && contentType != "image/png" && contentType != "image/webp" {
-			tools.BadRequestErrorHandler(w, errors.New("Only jpeg, png, and webp images are allowed"))
+			err := errors.New("Only jpeg, png, and webp images are allowed")
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			tools.BadRequestErrorHandler(w, err)
 			return
 		}
 
 		ext := filepath.Ext(handler.Filename)
 		objectName := productID + ext
 
-		path, putErr := platform.PutImage(objectName, file, handler.Size, contentType, r.Context())
+		path, putErr := platform.PutImage(objectName, file, handler.Size, contentType, ctx)
 		if putErr != nil {
+			span.RecordError(putErr)
+			span.SetStatus(codes.Error, putErr.Error())
 			msg := "We couldn't save your image"
 			tools.InternalServerErrorHandler(w, &msg)
 			return
 		}
 
-		if updateErr := db.UpdateProductImage(productID, path, userID, r.Context()); updateErr != nil {
+		if updateErr := db.UpdateProductImage(productID, path, userID, ctx); updateErr != nil {
 			if errors.Is(updateErr, db.ErrProductNotFound){
+				span.RecordError(db.ErrProductNotFound)
+				span.SetStatus(codes.Error, db.ErrProductNotFound.Error())
 				tools.NotFoundErrorHandler(w, "Product not found")
 				return
 			}
+
+			span.RecordError(updateErr)
+			span.SetStatus(codes.Error, putErr.Error())
 
 			// Real DB error
 			log.Error("Failed to update the product: ", updateErr)
 			tools.InternalServerErrorHandler(w, nil)
 			return
 		}
+
+		span.SetStatus(codes.Ok, "Saved image")
 
 		resp := tools.Message {
 			Message: "The image was saved successfully",
